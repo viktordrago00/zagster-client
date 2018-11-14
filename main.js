939c518b6cbabc99e5a0a1ba069057d6d6de5426
Viktor Medvinsky <medvinsv@oregonstate.edu>
2018-11-14 14:54:22 -0800

    putting data in console to verify we're getting it

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 $(updateView)
 
 function updateView() {
-    $.getJSON(BASE_URL + "/rides/count", updateRideCount)   
+    $.getJSON( BASE_URL + "/rides/count", updateRideCount)
+    $.getJSON( BASE_URL + "/rides/count/per_month", printData)
+    $.getJSON( BASE_URL + "/rides/count/per_year", printData)
 }
 
 function updateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
+}
+
+function printData(data) {
+    console.log(data)
 }
