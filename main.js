04028a9d6d6f5389b9b116ecd26083473dcff68b
Viktor Medvinsky <medvinsv@oregonstate.edu>
2018-11-19 15:14:31 -0800

    fixed console log

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const BASE_URL = "https://zagster-service.herokuapp.com"
 $(updateView)
 
 function updateView() {
-    $.getJSON( BASE_URL + "/rides/count", updateRideCount)
+    $.getJSON( BASE_URL + "/rides/count", printData)
     $.getJSON( BASE_URL + "/rides/count/per_month", printData)
     $.getJSON( BASE_URL + "/rides/count/per_year", printData)
+}
+
+function printData(data) {
+    console.log(data)
 }
